e46e39b6638452d64fc9106036c072aee73c6c24
chengohari <chengo267@gmail.com>
2021-01-06 02:25:49 +0200

    fixed post request

--- functions/index.js
+++ functions/index.js
@@ -15,7 +15,7 @@ getWaitingList = (docId) =>{
             const {doctorId}=doc.data();
             if(doctorId===docId){
                 const {time, token}=doc.data();
-                list.push({time: time, token:token});
+                list.push({'time': time, 'token':token});
             }
         });
 
@@ -32,51 +32,50 @@ getWaitingList = (docId) =>{
 exports.createWaiting = functions.firestore
     .document('WaitingList/{patientId}')
     .onCreate((snap, context) => {
-        console.log("oncreate1");
-      const newWaiting = snap.data();
-      console.log("on create2", newWaiting.patientName);
-      const doctorId = newWaiting.doctorId;
-      console.log("on create3 doc avail");
-    db.collection('Doctors').doc(doctorId).get()
-    .then(doc=> {const {waitingCounter} = doc.data();
-                if(waitingCounter===1){
-                    console.log("on create4 doc avail");
-                    request.post({
-                        headers: {'content-type' : 'application/jso'},
-                        url:     'https://exp.host/--/api/v2/push/send',
-                        body:    {"to":newWaiting.token,
-                                  "title": "BOOK A DOCTOR",
-                                  "body": "Your turn is now"}
-                      }, function(error, response, body){
-                        console.log(body);
-                      });
+        console.log("onCreate called")
+        const newWaiting = snap.data();
+        const doctorId = newWaiting.doctorId;
+        db.collection('Doctors').doc(doctorId).get()
+        .then(doc=> {const {waitingCounter} = doc.data();
+                    if(waitingCounter===1){
+                        request.post({
+                            headers: {'content-type' : 'application/json'},
+                            url:     'https://exp.host/--/api/v2/push/send',
+                            body:    {"to":newWaiting.token,
+                                    "title": "BOOK A DOCTOR",
+                                    "body": "Your turn is now"},
+                            json: true
+                        }, function(error, response, body){
+                            console.log("push response", body);
+                        });
+                    }
                     return 1;
-                }else{
-                    return 1;
-                }})
-    .catch(error=> console.log('Get Data Error'));
-      return 1;
+                })
+        .catch(error=> console.log('Get Data Error'));
+        return 1;
     });
 
 exports.deleteWaiting = functions.firestore
     .document('WaitingList/{patientId}')
     .onDelete((snap, context) => {
       const deletedWating = snap.data();
-      console.log("your deleted", newWaiting.patientName);
+      console.log("you deleted", deletedWating.patientName);
       var doctorId = deletedWating.doctorId;
       var waitingList = getWaitingList(doctorId);
       console.log(waitingList);
-      if(waitingList !== null){
+      if(waitingList.length > 0){
           var firstInList= waitingList[0];
-          var dateDelited = new Date(deletedWating.time);
           var dateFirst = new Date(firstInList.time);
-          if(dateFirst > dateDelited){
+          var dateDeleted = new Date(deletedWating.time);
+          console.log("delete log", dateDeleted, dateFirst);
+          if(dateFirst > dateDeleted){
             request.post({
-                headers: {'content-type' : 'application/jso'},
+                headers: {'content-type' : 'application/json'},
                 url:     'https://exp.host/--/api/v2/push/send',
                 body:    {"to":firstInList.token,
                           "title": "BOOK A DOCTOR",
-                          "body": "Your turn is now"}
+                          "body": "Your turn is now"},
+                json: true
               }, function(error, response, body){
                 console.log(body);
               });
